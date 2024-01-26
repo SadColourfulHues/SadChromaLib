d47405245f3f6f0c0a2ceba884df1f0b6aefd0f9
SadColourfulHues <46611929+SadColourfulHues@users.noreply.github.com>
2024-01-26 15:20:20 +0800

    Removed 'AI' and 'Animation' submodules from base

--- AI/Behaviour/AlwaysNode.cs
+++
@@ -1,9 +0,0 @@
-namespace SadChromaLib.AI.Behaviour;
-
-public partial class AlwaysNode : BehaviourNode
-{
-	public override Result Process(BehaviourContext context)
-	{
-		return Result.Success;
-	}
-}
