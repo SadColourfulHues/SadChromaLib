8c9dd78ec9a65ffa105fddf6c185fa9fbf848f2d
SadColourfulHues <46611929+SadColourfulHues@users.noreply.github.com>
2024-03-25 20:18:30 +0800

    (Timing module) update * Removed TimeSinceObserverDouble * TimeSinceObserver is now a struct

--- Utils/Timing/TimeSinceObserver.cs
+++ Utils/Timing/TimeSinceObserver.cs
@@ -1,15 +1,14 @@
-using Godot;
-
 namespace SadChromaLib.Utils.Timing;
 
 /// <summary> A TimeSinceObserver is a utility object that can be used to measure the time since an action was started. Useful for things like hold confirmation.</summary>
-public sealed partial class TimeSinceObserver : RefCounted
+public struct TimeSinceObserver
 {
     private ulong _startTime;
     private float _targetElapsedTime;
 
     public TimeSinceObserver(float targetElapsedTime = 1.0f)
     {
+        _startTime = 0;
         _targetElapsedTime = targetElapsedTime;
     }
 
@@ -26,13 +25,13 @@ public sealed partial class TimeSinceObserver : RefCounted
     }
 
     /// <summary> How much time has passed since the last 'Reset' method was called? (Returns elapsed time in seconds.) </summary>
-    public float TimeSince()
+    public readonly float TimeSince()
     {
         return (Time.GetTicksMsec() - _startTime) * 0.001f;
     }
 
     /// <summary> Has the elapsed time passed the target value? </summary>
-    public bool HasElapsed()
+    public readonly bool HasElapsed()
     {
         return TimeSince() >= _targetElapsedTime;
     }
