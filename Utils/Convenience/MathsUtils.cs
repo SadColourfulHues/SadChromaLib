7a5b6ecd10c5d7a4aa844c5f7ab36aa2b3d2c6c1
SadColourfulHues <46611929+SadColourfulHues@users.noreply.github.com>
2024-04-23 08:37:15 +0800

    [cleanup] Removed unused methods in MathsUtils

--- Utils/Convenience/MathsUtils.cs
+++ Utils/Convenience/MathsUtils.cs
@@ -1,5 +1,5 @@
-using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace SadChromaLib.Utils.Convenience;
 
@@ -8,92 +8,81 @@ public static class MathsUtils
 {
     #region Min/Max
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Clamp(float v, float min, float max) {
+        return MathF.Max(min, MathF.Min(max, v));
+    }
+
     /// <summary> Returns the smallest absolute value between a and b. </summary>
-    public static float AbsMin(float a, float b)
-    {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float AbsMin(float a, float b) {
         return MathF.Min(MathF.Abs(a), MathF.Abs(b));
     }
 
     /// <summary> Returns the smallest absolute value between a and b. </summary>
-    public static int AbsMin(int a, int b)
-    {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int AbsMin(int a, int b) {
         return Math.Min(Math.Abs(a), Math.Abs(b));
     }
 
     /// <summary> Returns the largest absolute value between a and b. </summary>
-    public static float AbsMax(float a, float b)
-    {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float AbsMax(float a, float b) {
         return MathF.Max(MathF.Abs(a), MathF.Abs(b));
     }
 
     /// <summary> Returns the largest absolute value between a and b. </summary>
-    public static int AbsMax(int a, int b)
-    {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int AbsMax(int a, int b) {
         return Math.Max(Math.Abs(a),  Math.Abs(b));
     }
 
     /// <summary> Checks if a is the largest value between a and b. (Can be configured to use AbsMax.) </summary>
-    public static bool IsMaxA(float a, float b, bool absolute = false)
-    {
-        if (absolute) {
-            return AbsMax(a, b) == MathF.Abs(a);
-        }
-
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsMaxA(float a, float b) {
         return MathF.Max(a, b) == a;
     }
 
     /// <summary> Checks if a is the largest value between a and b. (Can be configured to use AbsMax.) </summary>
-    public static bool IsMaxA(int a, int b, bool absolute = false)
-    {
-        if (absolute) {
-            return AbsMax(a, b) == Math.Abs(a);
-        }
-
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsMaxA(int a, int b) {
         return Math.Max(a, b) == a;
     }
 
-    /// <summary> Checks if a is the smallest value between a and b. (Can be configured to use AbsMin.) </summary>
-    public static bool IsMinA(float a, float b, bool absolute = false)
-    {
-        if (absolute) {
-            return AbsMin(a, b) == MathF.Abs(a);
-        }
+    /// <summary> [absolute value] Checks if a is the largest value between a and b. (Can be configured to use AbsMax.) </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsAbsMaxA(float a, float b) {
+        return MathF.Max(a, b) == a;
+    }
 
+    /// <summary> [absolute value] Checks if a is the largest value between a and b. (Can be configured to use AbsMax.) </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsAbsMaxA(int a, int b) {
+        return Math.Max(a, b) == a;
+    }
+
+    /// <summary> Checks if a is the smallest value between a and b.</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsMinA(float a, float b) {
         return MathF.Min(a, b) == a;
     }
 
-    /// <summary> Checks if a is the smallest value between a and b. (Can be configured to use AbsMin.) </summary>
-    public static bool IsMinA(int a, int b, bool absolute = false)
-    {
-        if (absolute) {
-            return AbsMin(a, b) == Math.Abs(a);
-        }
-
+    /// <summary> Checks if a is the smallest value between a and b. </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsMinA(int a, int b) {
         return Math.Min(a, b) == a;
     }
 
-    #endregion
-
-    #region Sign Utils
-
-    /// <summary> Restricts the value of x to the specified limit on both sides. (Limit is expected to be a positive floating point value.) </summary>
-    public static float Restrict(float x, float limit)
-    {
-        if (x > 0) {
-            return MathF.Min(x, limit);
-        }
-
-        return MathF.Max(x, -limit);
+    /// <summary> [absolute value] Checks if a is the smallest value between a and b.</summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsAbsMinA(float a, float b) {
+        return AbsMin(a, b) == a;
     }
 
-    /// <summary> Restricts the value of x to the specified limit on both sides. (Limit is expected to be a positive floating point value.) </summary>
-    public static int Restrict(int x, int limit)
-    {
-        if (x > 0) {
-            return Math.Min(x, limit);
-        }
-
-        return Math.Max(x, -limit);
+    /// <summary> [absolute value] Checks if a is the smallest value between a and b. </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsAbsMinA(int a, int b) {
+        return AbsMin(a, b) == a;
     }
 
     #endregion
