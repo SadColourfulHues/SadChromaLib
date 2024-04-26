8356c0be776330d8847ee1d369d2950420dbfb5d
SadColourfulHues <46611929+SadColourfulHues@users.noreply.github.com>
2024-04-26 21:39:03 +0800

    (feat) [MathsUtils] readded and rewritten previously-removed 3D utils 'IsFacing', and 'IsOnLeftSide'

--- Utils/Convenience/MathsUtils.cs
+++ Utils/Convenience/MathsUtils.cs
@@ -1,11 +1,55 @@
 using System;
 using System.Runtime.CompilerServices;
 
+using Godot;
+
 namespace SadChromaLib.Utils.Convenience;
 
 /// <summary> A collection of commonly-used maths-related methods. </summary>
 public static class MathsUtils
 {
+    #region 3D Utils
+
+    /// <summary>
+    /// Returns true if origin's forward is facing target
+    /// </summary>
+    /// <param name="forward">The forward direction of the origin</param>
+    /// <param name="origin">The origin's position</param>
+    /// <param name="target">The target's position</param>
+    /// <param name="threshold">How much room for error is allowed when performing the check</param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsFacing(Vector3 forward, Vector3 origin, Vector3 target, float threshold = 0.25f) {
+        return forward.Dot((target - origin).Normalized()) >= 1.0f - threshold;
+    }
+
+    /// <summary>
+    /// Returns true if the target position is on the origin's left side
+    /// </summary>
+    /// <param name="forward">The forward direction of the origin</param>
+    /// <param name="origin">The origin's position</param>
+    /// <param name="target">The target's position</param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsOnLeftSide(Vector3 forward, Vector3 origin, Vector3 target) {
+        return Vector3.Up.Dot(forward.Cross((target - origin).Normalized())) < 0.0f;
+    }
+
+    /// <summary>
+    /// Returns true if the target position is on the origin's left side
+    /// </summary>
+    /// <param name="forward">The forward direction of the origin</param>
+    /// <param name="up">Up vector override</param>
+    /// <param name="origin">The origin's position</param>
+    /// <param name="target">The target's position</param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsOnLeftSide(Vector3 forward, Vector3 up, Vector3 origin, Vector3 target) {
+        return up.Dot(forward.Cross((target - origin).Normalized())) < 0.0f;
+    }
+
+    #endregion
+
     #region Min/Max
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
