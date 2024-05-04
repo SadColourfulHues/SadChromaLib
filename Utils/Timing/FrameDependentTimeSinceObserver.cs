25cfe0b1ab7f09dbeecff6fdf37705de5ab6dedd
SadColourfulHues <46611929+SadColourfulHues@users.noreply.github.com>
2024-05-04 21:06:06 +0800

    (misc) [FrameDependentTimeSinceObserver] added docstrings + elapsed frame time getter

--- Utils/Timing/FrameDependentTimeSinceObserver.cs
+++ Utils/Timing/FrameDependentTimeSinceObserver.cs
@@ -26,23 +26,31 @@ public struct FrameDependentTimeSinceObserver
 
     #region Main Functions
 
+    /// <summary> Resets the timer. </summary>
     public void Reset() {
         _frameTime = 0.0f;
     }
 
+    /// <summary> Resets the timer and updates the target elapsed time. </summary>
     public void Reset(float targetElapsedTime)
     {
         _frameTime = 0.0f;
         _targetElapsedTime = targetElapsedTime;
     }
 
+    /// <summary> Increments the time with a given frame delta time </summary>
     public void Tick(float delta) {
         _frameTime += delta;
     }
 
-    public bool HasElapsed() {
-        return _frameTime >= _targetElapsedTime;
+    /// <summary> Returns the elapsed frame time since the last reset. </summary>
+    public readonly float TimeSince() {
+        return _frameTime;
     }
+
+    /// <summary> Has enough frame time has passed since the last reset? </summary>
+    public readonly bool HasElapsed()
+        => _frameTime >= _targetElapsedTime;
 
     #endregion
 }
